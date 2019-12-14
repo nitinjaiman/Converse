ffbbfe4587a4c4de90e244cf5d5b5336bd2e1bfb
Nitin Jaiman <jaimann@amazon.com>
2019-12-14 17:31:12 +0530

    first commit

---
+++ src/test/java/com/example/Converse/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Converse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
