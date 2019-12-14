92e0e925f55b7050723ad530b7d6d70b94e9a4c1
Nitin Jaiman <jaimann@amazon.com>
2019-12-14 18:06:37 +0530

    Added requrst mapping for /test

--- src/main/java/com/example/Converse/DemoApplication.java
+++ src/main/java/com/example/Converse/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class DemoApplication {
-    @RequestMapping("/")
+    @RequestMapping("/test")
     @ResponseBody
     String home() {
         return "Hello World!";
