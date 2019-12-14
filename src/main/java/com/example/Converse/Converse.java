059db206677fbba6c22751542eb76018e02ac2b5
Nitin Jaiman <jaimann@amazon.com>
2019-12-14 21:17:52 +0530

    Autowired

--- src/main/java/com/example/Converse/Converse.java
+++ src/main/java/com/example/Converse/Converse.java
@@ -3,6 +3,7 @@ package com.example.Converse;
 import com.example.Converse.model.User;
 import com.example.Converse.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class Converse {
 
+    @Autowired
     private UserService userService;
 
     @RequestMapping("/")
