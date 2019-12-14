738f5f589b3a7dfb5a960c5081099c58d8df5f39
Nitin Jaiman <jaimann@amazon.com>
2019-12-14 21:28:27 +0530

    Autowired

--- src/main/java/com/example/Converse/model/User.java
+++ src/main/java/com/example/Converse/model/User.java
@@ -2,16 +2,20 @@ package com.example.Converse.model;
 
 import lombok.Data;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Data
+@Table(name = "user")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long user_unique_id;
 
     private String user_id;
