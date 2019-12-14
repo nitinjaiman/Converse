ede837de43f3fe53421ac716c645ca3a3b5d9d72
Nitin Jaiman <jaimann@amazon.com>
2019-12-14 23:27:49 +0530

    Removed unused imports

--- src/main/java/com/example/Converse/model/User.java
+++ src/main/java/com/example/Converse/model/User.java
@@ -2,7 +2,6 @@ package com.example.Converse.model;
 
 import lombok.Data;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
--- src/main/java/com/example/Converse/spring/SpringConfiguration.java
+++ src/main/java/com/example/Converse/spring/SpringConfiguration.java
@@ -4,15 +4,12 @@ import com.example.Converse.repository.UserRepository;
 import com.example.Converse.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.orm.jpa.vendor.HibernateJpaSessionFactoryBean;
-
-import javax.persistence.EntityManagerFactory;
 
 @Configuration
 public class SpringConfiguration {
 
     @Bean
-    public UserService getUserService(UserRepository userRepository){
+    public UserService getUserService(UserRepository userRepository) {
         return new UserService(userRepository);
     }
 }
