50873792659e7c64c43bc0558cfb5e1ae721a20f
Nitin Jaiman <jaimann@amazon.com>
2019-12-14 21:14:47 +0530

    Added UserService bean

--- src/main/java/com/example/Converse/spring/SpringConfiguration.java
+++ src/main/java/com/example/Converse/spring/SpringConfiguration.java
@@ -1,5 +1,7 @@
 package com.example.Converse.spring;
 
+import com.example.Converse.repository.UserRepository;
+import com.example.Converse.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.orm.jpa.vendor.HibernateJpaSessionFactoryBean;
@@ -9,4 +11,8 @@ import javax.persistence.EntityManagerFactory;
 @Configuration
 public class SpringConfiguration {
 
+    @Bean
+    public UserService getUserService(UserRepository userRepository){
+        return new UserService(userRepository);
+    }
 }
